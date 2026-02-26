789ca4befa26b2c371f92fb615e995f1398463ba
Lau Salazar <144867734+Lau2S@users.noreply.github.com>
2026-02-26 15:28:52 -0500

    Create SalesDashboard component

---
+++ SalesDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SalesDashboard = () => {
+    return (
+        <div>
+            <h1>Sales Dashboard</h1>
+        </div>
+    );
+};
+
+export default SalesDashboard;
